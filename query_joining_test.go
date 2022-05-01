9f52fa7f287a47052254eecf3d68b951b1eac379
Taisuke Miyazaki <imishinist@gmail.com>
2022-05-01 23:20:04 +0900

    add inner_hits query

--- query_joining_test.go
+++ query_joining_test.go
@@ -5,12 +5,49 @@ import "testing"
 func TestNested(t *testing.T) {
 	runMapTests(t, []mapTest{
 		{
+			"InnerHits Query",
+			InnerHits(),
+			map[string]interface{}{
+				"inner_hits": map[string]interface{}{},
+			},
+		},
+		{
+			"InnerHits Query with options",
+			InnerHits().From(1).Size(10).Sort("field", "desc").Name("name"),
+			map[string]interface{}{
+				"inner_hits": map[string]interface{}{
+					"from": 1,
+					"size": 10,
+					"sort": Sort{
+						{
+							"field": map[string]interface{}{
+								"order": "desc",
+							},
+						},
+					},
+					"name": "name",
+				},
+			},
+		},
+		{
 			"Nested Query",
 			Nested("path", Term("field", "value")),
 			map[string]interface{}{
 				"nested": map[string]interface{}{
 					"path":  "path",
 					"query": Term("field", "value").Map(),
+				},
+			},
+		},
+		{
+			"Nested Query with inner_hits",
+			Nested("path", Term("field", "value")).InnerHits(Term("field", "value")).Name("name"),
+			map[string]interface{}{
+				"nested": map[string]interface{}{
+					"path":       "path",
+					"query":      Term("field", "value").Map(),
+					"inner_hits": Term("field", "value").Map(),
+					"_name":      "name",
 				},
 			},
 		},
